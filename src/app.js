cc991e5082be7063d3cabd13052741b8bcf3d480
FmMau <mfonseca_22@alu.uabcs.mx>
2026-05-29 17:26:11 -0700

    Reorder dotenv configuration and logging statements in app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
-const express = require('express');
+require('dotenv').config();
 
+const express = require('express');
 const cors = require('cors');
 
 const app = express();
@@ -15,6 +16,11 @@ app.get('/', (req, res) => {
   });
 
 });
+
+
+console.log('JWT:', process.env.JWT_SECRET);
+console.log('DATABASE:', process.env.DATABASE_URL);
+
 
 const PORT =
   process.env.PORT || 3000;
